d46b41441e85ecd24704155d3fd838505da01d62
Dhan Gabriel Mendoza <113577649+DMQKrul@users.noreply.github.com>
2024-03-21 16:40:35 +0800

    pause script

---
+++ Assets/Scripts/Manager/PauseMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+    public GameObject healthBar;
+    public GameObject pauseBtn;
+    public GameObject controls;
+
+    public void Pause()
+    {
+        PausePanel.SetActive(true);
+        healthBar.SetActive(false);
+        pauseBtn.SetActive(false);
+        controls.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        PausePanel.SetActive(false);
+        healthBar.SetActive(true);
+        pauseBtn.SetActive(true);
+        controls.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
