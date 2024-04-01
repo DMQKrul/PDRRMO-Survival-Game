929baa3dc6e8df329023ec7582e00691184a3e51
Dhan Gabriel Mendoza <113577649+DMQKrul@users.noreply.github.com>
2024-04-01 16:53:45 +0800

    Boss Almost Done

--- Assets/DirtBlockScript.cs
+++ Assets/DirtBlockScript.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class DirtBlockScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private AudioClip atkSoundClip;
+    public float atkVolume = 1f;
+
+    [SerializeField] private AudioClip atkSoundClip2;
+    public float atkVolume2 = 1f;
+
+    public GameObject spike1;
+    public GameObject spike2;
+    public GameObject spike3;
+    public Transform spike1Pos;
+    public Transform spike2Pos;
+    public Transform spike3Pos;
+    private Animator anim;
+    private GameObject player;
+    private CamShake shake;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        shake = GameObject.FindGameObjectWithTag("ScreenShake").GetComponent<CamShake>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exhausted()
     {
-        
+        anim.SetTrigger("Exhausted");
+    }
+
+    public void spikeSpawn1()
+    {
+        shake.CamShaking();
+        SoundFXManager.instance.PlaySoundFXClip(atkSoundClip2, transform, atkVolume2);
+        Instantiate(spike1, spike1Pos.position, Quaternion.identity);
+    }
+
+    public void spikeSpawn2()
+    {
+        shake.CamShaking();
+        SoundFXManager.instance.PlaySoundFXClip(atkSoundClip2, transform, atkVolume2);
+        Instantiate(spike2, spike2Pos.position, Quaternion.identity);
+    }
+    
+    public void spikeSpawn3()
+    {
+        shake.CamShaking();
+        SoundFXManager.instance.PlaySoundFXClip(atkSoundClip2, transform, atkVolume2);
+        Instantiate(spike3, spike3Pos.position, Quaternion.identity);
     }
 }
